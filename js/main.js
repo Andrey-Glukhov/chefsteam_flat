8b0f03a67d60719fc1c09fab1532c6633090bba9
Andrey-Glukhov <agi2000@mail.ru>
2021-06-03 18:09:08 +0300

    add scrollTo

--- js/main.js
+++ js/main.js
@@ -130,4 +130,62 @@
     var observer = new IntersectionObserver(callback, options);
     
     observer.observe(elementToObserve);
+   
+    var downButton = document.querySelector('.button_part');
+    if (downButton) {
+      downButton.addEventListener('click', function() {
+        document.querySelector('.offer').scrollIntoView({ 
+          behavior: 'smooth' 
+        });
+      })
+    }
+    var downButton = document.querySelector('.button_programm');
+    if (downButton) {
+      downButton.addEventListener('click', function() {
+        document.querySelector('.question').scrollIntoView({ 
+          behavior: 'smooth' 
+        });
+      })
+    }
+
+
+    // $('a[href*="#"]')
+    // // Remove links that don't actually link to anything
+    // .not('[href="#"]')
+    // .not('[href="#0"]')
+    // .click(function(event) {
+    //   // On-page links
+    //   if (
+    //     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    //     && 
+    //     location.hostname == this.hostname
+    //   ) {
+    //     // Figure out element to scroll to
+    //     var target = $(this.hash);
+    //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //     // Does a scroll target exist?
+    //     if (target.length) {
+    //       // Only prevent default if animation is actually gonna happen
+    //       event.preventDefault();
+    //       $('html, body').animate({
+    //         scrollTop: target.offset().top
+    //       }, 1000, function() {
+    //         // Callback after animation
+    //         // Must change focus!
+    //         var $target = $(target);
+    //         $target.focus();
+    //         if ($target.is(":focus")) { // Checking if the target was focused
+    //           return false;
+    //         } else {
+    //           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+    //           $target.focus(); // Set focus again
+    //         };
+    //       });
+    //     }
+    //   }
+    // });
+  
+
+
+
     }(jQuery) );
